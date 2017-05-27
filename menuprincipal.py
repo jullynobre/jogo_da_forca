8bc7fa8ba1e476d290fda13d2fff5d6a35f7d600
Jully Nobre <jullynobre1@gmail.com>
2017-05-27 16:49:22 -0300

    Set buttons

--- menuprincipal.py
+++ menuprincipal.py
@@ -5,19 +5,30 @@ root = Tk()
 root.geometry("800x600+50+50")
 
 s = Style()
+
 s.configure('Title.TFrame', background='#00008D')
-s.configure('Container.TFrame', background='#C8C4FF')
+s.configure('Container.TFrame')
+s.configure('T.TLabel', background="#00008D", foreground="white", font=("", 35))
+s.configure('MenuButtons.TButton', highlightbackground="#C8C4FF", foreground="#00008D", font=("", 15))
 
 titulo_frame = Frame(root, style='Title.TFrame')
 titulo_frame.place(height=100, width=800)
 titulo_frame.config()
 
-left_frame = Frame(root, style='Container.TFrame')
-left_frame.place(height=400, width=325, x=50, y=150)
-left_frame.config()
+titulo = Label(titulo_frame, text="JOGO DA FORCA", style='T.TLabel')
+titulo.pack(pady=20)
 
-right_frame = Frame(root, style='Container.TFrame')
-right_frame.place(height=400, width=325, x=425, y=150)
-right_frame.config()
+main_frame = Frame(root, style='Container.TFrame')
+main_frame.place(height=400, width=800, y=160)
+main_frame.config()
+
+novo_jogo = Button(main_frame, text="        Novo Jogo       ", style='MenuButtons.TButton')
+novo_jogo.pack(pady=20)
+continuar_jogo = Button(main_frame, text="     Continuar Jogo    ", style='MenuButtons.TButton')
+continuar_jogo.pack(pady=20)
+placares_jogo = Button(main_frame, text="         Placares         ", style='MenuButtons.TButton')
+placares_jogo.pack(pady=20)
+gerenciar_palavras = Button(main_frame, text="  Gerenciar Palavras ", style='MenuButtons.TButton')
+gerenciar_palavras.pack(pady=20)
 
 root.mainloop()
